db96339ed918ba1b681477b276235b4f7cd589ef
Dylan Armstrong <dylanarmstrong116@gmail.com>
2018-07-07 08:10:23 -0500

    finished up hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,3 +1,8 @@
+--[[
+I busted the right 3mm or so of my screen
+so everything is designed to handle that
+--]]
+
 hs.window.animationDuration = 0
 
 hs.hotkey.bind({'alt', 'ctrl'}, 'Left', function()
@@ -38,4 +43,58 @@ hs.hotkey.bind({'alt', 'ctrl'}, 'Return', function()
   f.h = max.h
   win:setFrame(f)
 end)
+
+hs.hotkey.bind({'alt', 'ctrl'}, 'Up', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w - 10
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({'alt', 'ctrl'}, 'Down', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y + (max.h / 2)
+  f.w = max.w - 10
+  f.h = max.h / 2
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({'alt', 'ctrl'}, 'R', function()
+  hs.reload()
+end)
+
+--[[
+--
+-- nifty caffeine replacement from docs
+-- TODO: Find higher res coffee icon to replace caffinate.
+--
+caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+  if state then
+    caffeine:setTitle('AWAKE')
+  else
+    caffeine:setTitle('SLEEPY')
+  end
+end
+
+function caffeineClicked()
+  setCaffeineDisplay(hs.caffeinate.toggle('displayIdle'))
+end
+
+if caffeine then
+  caffeine:setClickCallback(caffeineClicked)
+  setCaffeineDisplay(hs.caffeinate.get('displayIdle'))
+end
+--]]
 
