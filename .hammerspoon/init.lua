389fcf323833b93cd6db05c84a179781fc316e39
Dylan Armstrong <dylanarmstrong116@gmail.com>
2018-07-07 07:47:02 -0500

    using hammerspoon for window mgmt

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,41 @@
+hs.window.animationDuration = 0
+
+hs.hotkey.bind({'alt', 'ctrl'}, 'Left', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({'alt', 'ctrl'}, 'Right', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = (max.x + (max.w / 2)) - 10
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind({'alt', 'ctrl'}, 'Return', function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w - 10
+  f.h = max.h
+  win:setFrame(f)
+end)
+
