799bfe69f34d3c0dd5d7a32387f40f5be631323d
Dylan Armstrong <dylanarmstrong116@gmail.com>
2018-07-08 12:29:30 -0500

    caffeine replacement, cleaner push code

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -7,68 +7,72 @@ hs.window.animationDuration = 0
 
 hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', hs.reload):start()
 
--- The amount of pixels I've broken on the right of my screen..
-local screenBreak = 9
+-- Column of 9 pixels I've broken on the right of my screen..
+local screenBreak = 0.00703125
+-- 1280 * x - 9 = 0, x = 0.00703125
 
-function move(pos)
+function push(x, y, w, h)
+
   local win = hs.window.frontmostWindow()
   local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
+  max.w = max.w - (max.w * screenBreak)
 
-  local poses = {
-    full = {
-      x = max.x,
-      y = max.y,
-      w = max.w - screenBreak,
-      h = max.h
-    },
+  f.x = max.x + (max.w * x)
+  f.y = max.y + (max.h * y)
+  f.w = max.w * w
+  f.h = max.h * h
 
-    up = {
-      x = max.x,
-      y = max.y,
-      w = max.w - screenBreak,
-      h = max.h / 2
-    },
-
-    right = {
-      x = (max.x + (max.w / 2)) - screenBreak,
-      y = max.y,
-      w = max.w / 2,
-      h = max.h
-    },
-
-    down = {
-      x = max.x,
-      y = max.y + (max.h / 2),
-      w = max.w - screenBreak,
-      h = max.h / 2
-    },
-
-    left = {
-      x = max.x,
-      y = max.y,
-      w = max.w / 2,
-      h = max.h
-    },
-  }
-
-  win:setFrame(poses[pos])
+  win:setFrame(f)
 end
 
 local mod = { 'alt', 'ctrl' }
+
 hs.fnutils.each({
-  { key = 'Return', pos = 'full' },
-  { key = 'Up', pos = 'up' },
-  { key = 'Right', pos = 'right' },
-  { key = 'Down', pos = 'down' },
-  { key = 'Left', pos = 'left' }
+  { key = 'Return', pos = {   0,   0,   1,   1 }},  -- Full
+  { key = 'Up',     pos = {   0,   0,   1, 0.5 }},  -- Top Half
+  { key = 'Right',  pos = { 0.5,   0, 0.5,   1 }},  -- Right Half
+  { key = 'Down',   pos = {   0, 0.5,   1, 0.5 }},  -- Bottom Half
+  { key = 'Left',   pos = {   0,   0, 0.5,   1 }},  -- Left Half
+  { key = 'Y',      pos = {   0,   0, 0.5, 0.5 }},  -- Upper Left Half
+  { key = 'U',      pos = { 0.5,   0, 0.5, 0.5 }},  -- Upper Right Half
+  { key = 'B',      pos = {   0, 0.5, 0.5, 0.5 }},  -- Bottom Left Half
+  { key = 'N',      pos = { 0.5, 0.5, 0.5, 0.5 }}   -- Bottom Right Half
 }, function(obj)
   hs.hotkey.bind(mod, obj.key, function()
-    move(obj.pos)
+    push(table.unpack(obj.pos))
   end)
 end)
 
--- heh
+hs.fnutils.each({
+  { key = '1', app = 'iTerm' },
+  { key = '2', app = 'Safari' },
+  { key = '3', app = 'Messages' },
+  { key = '4', app = 'Finder' }
+}, function(obj)
+  hs.hotkey.bind(mod, obj.key, function()
+    hs.application.launchOrFocus(obj.app)
+  end)
+end)
+
+local caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+  if state then
+    caffeine:setIcon('./caffeine-on.pdf')
+  else
+    caffeine:setIcon('./caffeine-off.pdf')
+  end
+end
+
+function caffeineClicked()
+  setCaffeineDisplay(hs.caffeinate.toggle('displayIdle'))
+end
+
+if caffeine then
+  caffeine:setClickCallback(caffeineClicked)
+  setCaffeineDisplay(hs.caffeinate.get('displayIdle'))
+end
+
 hs.alert.show('Hammerspoon loaded 👍')
 
