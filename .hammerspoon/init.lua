ea41953846026bbdcf11e45229ac250e930ded6f
Dylan Armstrong <dylanarmstrong116@gmail.com>
2018-07-08 09:07:22 -0500

    unbroke hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -3,64 +3,72 @@ I busted the right 3mm or so of my screen
 so everything is designed to handle that
 --]]
 
--- The amount of pixels I've broken on the right of my screen..
-local screenBreak = 9
+hs.window.animationDuration = 0
 
 hs.pathwatcher.new(os.getenv('HOME') .. '/.hammerspoon/', hs.reload):start()
 
-hs.window.animationDuration = 0
+-- The amount of pixels I've broken on the right of my screen..
+local screenBreak = 9
 
 function move(pos)
-  local win = hs.window.focusedWindow()
+  local win = hs.window.frontmostWindow()
+  local f = win:frame()
   local screen = win:screen()
   local max = screen:frame()
-  local poss = {
+
+  local poses = {
     full = {
       x = max.x,
       y = max.y,
       w = max.w - screenBreak,
       h = max.h
     },
-    left = {
-      x = max.x,
-      y = max.y,
-      w = max.w / 2,
-      h = max.h
-    },
-    right = {
-      x = (max.x + (max.w / 2)) - screenBreak,
-      y = max.y,
-      w = max.w / 2,
-      h = max.h
-    },
+
     up = {
       x = max.x,
       y = max.y,
       w = max.w - screenBreak,
       h = max.h / 2
     },
+
+    right = {
+      x = (max.x + (max.w / 2)) - screenBreak,
+      y = max.y,
+      w = max.w / 2,
+      h = max.h
+    },
+
     down = {
       x = max.x,
       y = max.y + (max.h / 2),
       w = max.w - screenBreak,
       h = max.h / 2
-    }
+    },
+
+    left = {
+      x = max.x,
+      y = max.y,
+      w = max.w / 2,
+      h = max.h
+    },
   }
 
-  local f = poss[pos]
-  return function()
-    win:setFrame(f)
-  end
+  win:setFrame(poses[pos])
 end
 
 local mod = { 'alt', 'ctrl' }
 hs.fnutils.each({
-  { key = 'return', pos = 'full' },
-  { key = 'left', pos = 'left' },
-  { key = 'right', pos = 'right' },
-  { key = 'up', pos = 'up' },
-  { key = 'down', pos = 'down' }
+  { key = 'Return', pos = 'full' },
+  { key = 'Up', pos = 'up' },
+  { key = 'Right', pos = 'right' },
+  { key = 'Down', pos = 'down' },
+  { key = 'Left', pos = 'left' }
 }, function(obj)
-  hs.hotkey.new(mod, obj.key, move(obj.pos)):enable()
+  hs.hotkey.bind(mod, obj.key, function()
+    move(obj.pos)
+  end)
 end)
+
+-- heh
+hs.alert.show('Hammerspoon loaded 👍')
 
