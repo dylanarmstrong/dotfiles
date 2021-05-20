9a9ddf927bd488be936c7a4c1887a75c21099405
Dylan Armstrong <dylan@dylan.is>
2021-05-20 13:08:37 -0500

    chore: forgot a prefix

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,7 +1,7 @@
 local install_path = vim.fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
-  fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
+  vim.fn.system({ 'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path })
   vim.api.nvim_command('packadd packer.nvim')
 end
 
