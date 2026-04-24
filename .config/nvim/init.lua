113f55305008cdae3720cd04b8065e886edbd883
Dylan Armstrong <dylan@dylan.is>
2026-04-24 06:11:03 +0200

    chore: pin commits in nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,11 +1,69 @@
 local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
 
+local plugin_commits = {
+  ['brenoprata10/nvim-highlight-colors'] = 'e2cb22089cc2358b2b995c09578224f142de6039',
+  ['catppuccin/nvim'] = '426dbebe06b5c69fd846ceb17b42e12f890aedf1',
+  ['digitaltoad/vim-pug'] = 'ea39cd942cf3194230cf72bfb838901a5344d3b3',
+  ['dmmulroy/ts-error-translator.nvim'] = '558abff11b9e8f4cefc0de09df780c56841c7a4b',
+  ['echasnovski/mini.nvim'] = '5849ef04c32a6a8e55957b946c0a275801d87530',
+  ['folke/flash.nvim'] = 'fcea7ff883235d9024dc41e638f164a450c14ca2',
+  ['folke/lazy.nvim'] = '306a05526ada86a7b30af95c5cc81ffba93fef97',
+  ['folke/lazydev.nvim'] = 'ff2cbcba459b637ec3fd165a2be59b7bbaeedf0d',
+  ['folke/trouble.nvim'] = 'bd67efe408d4816e25e8491cc5ad4088e708a69a',
+  ['hedyhli/outline.nvim'] = 'c293eb56db880a0539bf9d85b4a27816960b863e',
+  ['ibhagwan/fzf-lua'] = 'ffa44ee9470743a7697d28df3a1a216fdfe2b09d',
+  ['jmbuhr/otter.nvim'] = 'a455e68a99d395889ab30a25ac3846a135e93c46',
+  ['julienvincent/hunk.nvim'] = 'a73c34934e87af4f2e615fea3a57ee28b2dceec8',
+  ['kaarmu/typst.vim'] = '1d5436c0f55490893892441c0eca55e6cdf4916c',
+  ['lewis6991/gitsigns.nvim'] = '6d808f99bd63303646794406e270bd553ad7792e',
+  ['lukas-reineke/indent-blankline.nvim'] = 'd28a3f70721c79e3c5f6693057ae929f3d9c0a03',
+  ['mfussenegger/nvim-lint'] = 'eab58b48eb11d7745c11c505e0f3057165902461',
+  ['mrcjkb/rustaceanvim'] = 'c9eeb10fc3ca48cc1c75b79b420de67433629ba5',
+  ['MunifTanjim/nui.nvim'] = 'de740991c12411b663994b2860f1a4fd0937c130',
+  ['neovim/nvim-lspconfig'] = '81878de76c0de4ce289513734ad80c31ec7871b8',
+  ['nmac427/guess-indent.nvim'] = '84a4987ff36798c2fc1169cbaff67960aed9776f',
+  ['nvim-lua/plenary.nvim'] = '74b06c6c75e4eeb3108ec01852001636d85a932b',
+  ['nvim-lualine/lualine.nvim'] = 'a905eeebc4e63fdc48b5135d3bf8aea5618fb21c',
+  ['nvim-neo-tree/neo-tree.nvim'] = '4d0828d95adaf4250c1373d2cd9318d3509712b7',
+  ['nvim-treesitter/nvim-treesitter'] = '4916d6592ede8c07973490d9322f187e07dfefac',
+  ['nvim-treesitter/nvim-treesitter-context'] = 'b0c45cefe2c8f7b55fc46f34e563bc428ef99636',
+  ['saghen/blink.cmp'] = '78336bc89ee5365633bcf754d93df01678b5c08f',
+  ['stevearc/conform.nvim'] = 'dca1a190aa85f9065979ef35802fb77131911106',
+  ['stevearc/oil.nvim'] = '0fcc83805ad11cf714a949c98c605ed717e0b83e',
+  ['towolf/vim-helm'] = '2c8525fd98e57472769d137317bca83e477858ce',
+  ['tpope/vim-fugitive'] = '3b753cf8c6a4dcde6edee8827d464ba9b8c4a6f0',
+  ['tpope/vim-repeat'] = '65846025c15494983dafe5e3b46c8f88ab2e9635',
+  ['tpope/vim-speeddating'] = 'c17eb01ebf5aaf766c53bab1f6592710e5ffb796',
+  ['vigoux/templar.nvim'] = '58e5bf01e7badedeff0f25d00f8cbbc537934f28',
+}
+
+local function pin(plugin)
+  local commit = plugin_commits[plugin]
+  if not commit then
+    error('Missing pinned commit for ' .. plugin)
+  end
+  return commit
+end
+
+local lazycommit = pin('folke/lazy.nvim')
+
 if not vim.uv.fs_stat(lazypath) then
   local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
-  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', '--branch=stable', lazyrepo, lazypath })
+  local out = vim.fn.system({ 'git', 'clone', '--filter=blob:none', lazyrepo, lazypath })
   if vim.v.shell_error ~= 0 then
     vim.api.nvim_echo({
       { 'Failed to clone lazy.nvim:\n', 'ErrorMsg' },
+      { out, 'WarningMsg' },
+      { '\nPress any key to exit...' },
+    }, true, {})
+    vim.fn.getchar()
+    os.exit(1)
+  end
+
+  out = vim.fn.system({ 'git', '-C', lazypath, 'checkout', lazycommit })
+  if vim.v.shell_error ~= 0 then
+    vim.api.nvim_echo({
+      { 'Failed to checkout lazy.nvim commit:\n', 'ErrorMsg' },
       { out, 'WarningMsg' },
       { '\nPress any key to exit...' },
     }, true, {})
@@ -37,9 +95,15 @@ vim.opt.numberwidth = 4
 local flavor = 'mocha'
 
 require('lazy').setup({
+  {
+    'folke/lazy.nvim',
+    commit = lazycommit,
+  },
+
   -- Styling
   {
     'catppuccin/nvim',
+    commit = pin('catppuccin/nvim'),
     lazy = false,
     priority = 1000,
     opts = {
@@ -91,12 +155,14 @@ require('lazy').setup({
 
   {
     'lukas-reineke/indent-blankline.nvim',
+    commit = pin('lukas-reineke/indent-blankline.nvim'),
     main = 'ibl',
     opts = {},
   },
 
   {
     'brenoprata10/nvim-highlight-colors',
+    commit = pin('brenoprata10/nvim-highlight-colors'),
     event = 'BufReadPre',
     opts = {
       render = 'virtual',
@@ -110,12 +176,14 @@ require('lazy').setup({
   -- Works more consistently than vim-sleuth I've found
   {
     'nmac427/guess-indent.nvim',
+    commit = pin('nmac427/guess-indent.nvim'),
     opts = {},
   },
 
   -- Mini
   {
     'echasnovski/mini.nvim',
+    commit = pin('echasnovski/mini.nvim'),
     config = function()
       -- Comments with gc
       require('mini.comment').setup()
@@ -128,6 +196,7 @@ require('lazy').setup({
   -- Git
   {
     'tpope/vim-fugitive',
+    commit = pin('tpope/vim-fugitive'),
     keys = {
       { '<leader>gd', '<cmd>Gdiff<cr>' },
     },
@@ -135,14 +204,16 @@ require('lazy').setup({
 
   {
     'lewis6991/gitsigns.nvim',
+    commit = pin('lewis6991/gitsigns.nvim'),
     opts = {},
   },
 
   -- Finder
   {
     'ibhagwan/fzf-lua',
+    commit = pin('ibhagwan/fzf-lua'),
     dependencies = {
-      'echasnovski/mini.nvim',
+      { 'echasnovski/mini.nvim', commit = pin('echasnovski/mini.nvim') },
     },
     keys = {
       { '<C-p>', '<cmd>FzfLua files<cr>' },
@@ -173,6 +244,7 @@ require('lazy').setup({
   -- Completion
   {
     'saghen/blink.cmp',
+    commit = pin('saghen/blink.cmp'),
     dependencies = {},
     build = 'cargo build --release',
     opts = {
@@ -238,6 +310,7 @@ require('lazy').setup({
 
   {
     'neovim/nvim-lspconfig',
+    commit = pin('neovim/nvim-lspconfig'),
     config = function(_, opts)
       for server, config in pairs(opts.servers) do
         vim.lsp.config(server, config)
@@ -374,14 +447,18 @@ require('lazy').setup({
   },
 
   -- Translate TS Errors to English
-  { 'dmmulroy/ts-error-translator.nvim' },
+  { 'dmmulroy/ts-error-translator.nvim', commit = pin('dmmulroy/ts-error-translator.nvim') },
 
   -- This seems less problematic than efm-langserver
   -- For formatting on save
   {
     'stevearc/conform.nvim',
+    commit = pin('stevearc/conform.nvim'),
     opts = {
       formatters = {
+        blank_prettier = {
+          command = 'prettier',
+        },
         stylua = {
           args = {
             '--search-parent-directories',
@@ -438,6 +515,7 @@ require('lazy').setup({
   -- Linting
   {
     'mfussenegger/nvim-lint',
+    commit = pin('mfussenegger/nvim-lint'),
     config = function()
       local lint = require('lint')
       lint.linters_by_ft = {
@@ -460,6 +538,7 @@ require('lazy').setup({
   -- Treesitter for fancy syntax
   {
     'nvim-treesitter/nvim-treesitter',
+    commit = pin('nvim-treesitter/nvim-treesitter'),
     build = ':TSUpdate',
     dependencies = {},
     lazy = false,
@@ -488,8 +567,9 @@ require('lazy').setup({
 
   {
     'nvim-treesitter/nvim-treesitter-context',
+    commit = pin('nvim-treesitter/nvim-treesitter-context'),
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
+      { 'nvim-treesitter/nvim-treesitter', commit = pin('nvim-treesitter/nvim-treesitter') },
     },
     name = 'treesitter-context',
     opts = {
@@ -500,6 +580,7 @@ require('lazy').setup({
   -- Embedded treesitter
   {
     'jmbuhr/otter.nvim',
+    commit = pin('jmbuhr/otter.nvim'),
     init = function()
       vim.api.nvim_create_autocmd('BufEnter', {
         pattern = 'markdown',
@@ -509,7 +590,7 @@ require('lazy').setup({
       })
     end,
     dependencies = {
-      'nvim-treesitter/nvim-treesitter',
+      { 'nvim-treesitter/nvim-treesitter', commit = pin('nvim-treesitter/nvim-treesitter') },
     },
     ft = 'markdown',
     opts = {},
@@ -517,6 +598,7 @@ require('lazy').setup({
 
   {
     'hedyhli/outline.nvim',
+    commit = pin('hedyhli/outline.nvim'),
     opts = {},
     keys = {
       { '<leader>o', '<cmd>Outline<cr>' },
@@ -526,24 +608,28 @@ require('lazy').setup({
   -- Typst support
   {
     'kaarmu/typst.vim',
+    commit = pin('kaarmu/typst.vim'),
     ft = { 'typst' },
   },
 
   -- Pug support
   {
     'digitaltoad/vim-pug',
+    commit = pin('digitaltoad/vim-pug'),
     ft = { 'pug' },
   },
 
   -- Helm support
   {
     'towolf/vim-helm',
+    commit = pin('towolf/vim-helm'),
     ft = { 'helm' },
   },
 
   -- Lua
   {
     'folke/lazydev.nvim',
+    commit = pin('folke/lazydev.nvim'),
     ft = 'lua',
     opts = {
       library = {
@@ -556,26 +642,35 @@ require('lazy').setup({
   -- Rust support
   {
     'mrcjkb/rustaceanvim',
+    commit = pin('mrcjkb/rustaceanvim'),
     ft = 'rust',
     lazy = false,
   },
 
   {
     'julienvincent/hunk.nvim',
+    commit = pin('julienvincent/hunk.nvim'),
     cmd = { 'DiffEditor' },
   },
 
   -- Better repeating
-  'tpope/vim-repeat',
+  {
+    'tpope/vim-repeat',
+    commit = pin('tpope/vim-repeat'),
+  },
 
   -- Better c-a / c-x
-  'tpope/vim-speeddating',
+  {
+    'tpope/vim-speeddating',
+    commit = pin('tpope/vim-speeddating'),
+  },
 
   -- Status line
   {
     'nvim-lualine/lualine.nvim',
+    commit = pin('nvim-lualine/lualine.nvim'),
     dependencies = {
-      'catppuccin/nvim',
+      { 'catppuccin/nvim', commit = pin('catppuccin/nvim') },
     },
     opts = function(_, opts)
       local colors = require('catppuccin.palettes').get_palette(flavor)
@@ -661,10 +756,11 @@ require('lazy').setup({
   -- File browser
   {
     'nvim-neo-tree/neo-tree.nvim',
+    commit = pin('nvim-neo-tree/neo-tree.nvim'),
     dependencies = {
-      'MunifTanjim/nui.nvim',
-      'echasnovski/mini.nvim',
-      'nvim-lua/plenary.nvim',
+      { 'MunifTanjim/nui.nvim', commit = pin('MunifTanjim/nui.nvim') },
+      { 'echasnovski/mini.nvim', commit = pin('echasnovski/mini.nvim') },
+      { 'nvim-lua/plenary.nvim', commit = pin('nvim-lua/plenary.nvim') },
     },
     keys = {
       { '<C-n>', '<cmd>Neotree toggle<cr>' },
@@ -715,8 +811,9 @@ require('lazy').setup({
   -- File Creation
   {
     'stevearc/oil.nvim',
+    commit = pin('stevearc/oil.nvim'),
     dependencies = {
-      'echasnovski/mini.nvim',
+      { 'echasnovski/mini.nvim', commit = pin('echasnovski/mini.nvim') },
     },
     keys = {
       { '<leader>n', '<cmd>Oil<cr>' },
@@ -728,6 +825,7 @@ require('lazy').setup({
   -- Diagnostics
   {
     'folke/trouble.nvim',
+    commit = pin('folke/trouble.nvim'),
     keys = {
       { '<leader>e', '<cmd>Trouble diagnostics toggle<cr>' },
     },
@@ -739,6 +837,7 @@ require('lazy').setup({
   -- Templates
   {
     'vigoux/templar.nvim',
+    commit = pin('vigoux/templar.nvim'),
     config = function()
       local templar = require('templar')
       templar.register('*.html')
@@ -750,6 +849,7 @@ require('lazy').setup({
   -- Movement
   {
     'folke/flash.nvim',
+    commit = pin('folke/flash.nvim'),
     event = 'VeryLazy',
     keys = {
       {
